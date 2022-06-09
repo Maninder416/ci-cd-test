7951a033ac761cda6da3e8a7326df2d4fc19e512
Maninder416 <Maninder.singh@fullstacklabs.ca>
2022-06-09 15:13:49 -0400

    adding support for aws spring boot application with api gateway

--- src/main/java/io/reactivestax/SpringAwsApplication.java
+++ src/main/java/io/reactivestax/SpringAwsApplication.java
@@ -1,10 +1,30 @@
 package io.reactivestax;
 
+import io.reactivestax.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/book")
 public class SpringAwsApplication {
+
+	private List<Book> books= new ArrayList<>();
+
+	@PostMapping
+	public Book addBook(@RequestBody Book book){
+		 books.add(book);
+		 return book;
+	}
+
+	@GetMapping
+	public List<Book> getBook(){
+		return books;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAwsApplication.class, args);

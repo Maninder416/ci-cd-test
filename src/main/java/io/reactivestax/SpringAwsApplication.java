37d8e1c6c82fc34b6ff52ebdb7ea4c78bb2770aa
Maninder416 <Maninder.singh@fullstacklabs.ca>
2022-06-09 16:28:11 -0400

    adding support for aws spring boot application with ecs and fargate

--- src/main/java/io/reactivestax/SpringAwsApplication.java
+++ src/main/java/io/reactivestax/SpringAwsApplication.java
@@ -1,6 +1,5 @@
 package io.reactivestax;
 
-import io.reactivestax.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -10,20 +9,19 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/")
 public class SpringAwsApplication {
 
-	private List<Book> books= new ArrayList<>();
+	@GetMapping
+	public String applicationStatus() {
+		return "Application is up and running";
 
-	@PostMapping
-	public Book addBook(@RequestBody Book book){
-		 books.add(book);
-		 return book;
 	}
 
-	@GetMapping
-	public List<Book> getBook(){
-		return books;
+	@GetMapping("/{name}")
+	public String welcome(@PathVariable String name) {
+		return "Hi" + name+ "Welcome to java techie AWS ECS Example";
+
 	}
 
 	public static void main(String[] args) {
